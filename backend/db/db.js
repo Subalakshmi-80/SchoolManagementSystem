74f6186b68205ef6973b11ea62c18a0256318bb7
Subalakshmi-80 <subaproj3440@gmail.com>
2026-07-21 11:00:52 +0530

    Updated database configuration for deployment

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,11 +1,28 @@
 const {Pool} = require('pg');
 
-const pool = new Pool({
+let pool;
+
+if(process.env.DATABASE_URL){
+    pool = new Pool({
+        connectionString:process.env.DATABASE_URL,
+        ssl:{
+            rejectUnauthorized:false,
+        },
+    })
+}
+else{
+ pool = new Pool({
     host:process.env.DB_HOST,
     port:process.env.DB_PORT,
     user:process.env.DB_USER,
     database:process.env.DB_NAME,
-    password:process.env.DB_PASSWORD
+    password:process.env.DB_PASSWORD,
+
 });
+}
+
+
+
+
 
 module.exports = pool;
